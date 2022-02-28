b307f64deb7a218731b323212b283624037cc9b5
imaginelenses <imaginelenses@gmail.com>
2022-02-28 17:54:08 +0530

    Migrated to bootstrap v

--- fullscreen.js
+++ fullscreen.js
@@ -1,45 +1,56 @@
-/*
- * Renders images in fullscreen when clicked on
- *
- * Usage:
- * Add the following to the body
- * '<div class="modal fade" id="fullscreen" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog modal-dialog-centered"></div></div>
- *
- * Image tag format
- * <img class="thumbnail" src="..." alt="...">
- * To add more classes to image tag thumbnail[] index should be modified accordingly
- *
- * Add to stylesheet:
- * .thumbnail { object-fit: cover; }
- * .fullscreen { max-width: 90vw; max-height: 90vh; }
- */
-
 // Waits for the DOM to be loaded
 document.addEventListener('DOMContentLoaded', function() {
+
+    // Array of all images
+    let images = [];
 
     // Selects all images with class "thumbnail"
     document.querySelectorAll('.thumbnail').forEach(item=> {
 
+        // Store paths of images
+        images.push(item.attributes.getNamedItem('src').value);
+
+        // Gets the image node that clicked on
+        let thumbnail = item.parentElement.innerHTML;
+        thumbnail = String(thumbnail).trim().split(' ');
+
+        // Adds the image node to the HTML node added above after replacing class "thumbnail" with "fullscreen"
+        let fullscreen = document.querySelector('.carousel-inner');
+        let temp = thumbnail[0] + ' class="mx-auto fullscreen" ';
+
+        let n = thumbnail.length;
+        for (let i = 2; i < n; i++) {
+            temp = temp + thumbnail[i];
+        }
+        fullscreen.innerHTML = fullscreen.innerHTML + '<div class="carousel-item">' + temp + '</div>';
+        
         // Adds a click event listner to each of thoses images
         item.addEventListener('click', event=> {
+            
+            // Get path of image 
+            let image = event.target.attributes.getNamedItem('src').value;
+            
+            // Get index of image
+            let index = images.indexOf(image);
 
-            // Gets the image node that clicked on
-            let thumbnail = item.parentElement.innerHTML;
-            thumbnail = String(thumbnail).trim().split(' ');
-
-            // Adds the image node to the HTML node added above after replacing class "thumbnail" with "fullscreen"
-            let fullscreen = document.querySelector('.modal-dialog');
-            let temp = thumbnail[0] + ' class="mx-auto fullscreen" ';
-
-            let n = thumbnail.length;
-            for (let i = 2; i < n; i++)
-            {
-                temp = temp + thumbnail[i];
-            }
-            fullscreen.innerHTML = temp;
+            // Activate image in carousel
+            $('#carouselBanner').carousel(index);
 
             // Renders image in fullscreen
             $('#fullscreen').modal('show');
         });
+    });
+
+    // Set first carousel item active
+    $('.carousel-item').first().addClass('active');
+
+    // Handel keyboard input for carousel
+    document.querySelector('.modal').addEventListener('keydown', event => {
+        if (event.keyCode === 37) {
+            $('#carouselBanner').carousel('prev');
+        }
+        if (event.keyCode === 39) {
+            $('#carouselBanner').carousel('next');
+        }
     });
 });
