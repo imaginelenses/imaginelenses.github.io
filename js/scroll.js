0deaba589e10d4718cf6ed65c3d82914760c2bf4
imaginelenses <imaginelenses@users.noreply.github.com>
2026-04-13 04:22:32 +0000

    deploy: dde7843eba73b856579ca49f15290c9972ce4fd6

--- js/scroll.js
+++ js/scroll.js
@@ -8,35 +8,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const sectionImages = document.querySelectorAll('.sectionImage')
     
-    let init = 1
-    if (document.documentElement.clientHeight > sectionImages[0].height) {
-        init = 1.1
+    if (sectionImages.length > 0) {
+        let init = 1
+        if (document.documentElement.clientHeight > sectionImages[0].height) {
+            init = 1.1
+        }
+
+        const factor = 7000
+
+        window.scrollTo(1, 1)
+
+        document.addEventListener('scroll', (e) => {
+            let scroll = window.scrollY
+            if (scroll === 0) {
+                return 
+            }
+            
+            const applyTransform = (img, scale) => {
+                const flip = img.classList.contains('mirroredImage') ? 'scaleX(-1) ' : ''
+                img.style.transform = `${flip}scale(${scale})`
+            }
+
+            let offset = 0
+            applyTransform(sectionImages[0], init + ((scroll - offset) / factor))
+            offset = offset + sectionImages[0].height
+
+            for (let i = 1, len = sectionImages.length; i < len; i++) {
+                applyTransform(sectionImages[i], init + ((scroll - offset) / factor))
+                offset = offset + sectionImages[i].height
+            }
+        })
     }
-
-    const factor = 7000
-
-    window.scrollTo(1, 1)
-
-    document.addEventListener('scroll', (e) => {
-        let scroll = window.scrollY
-        if (scroll === 0) {
-            return 
-        }
-        
-        const applyTransform = (img, scale) => {
-            const flip = img.classList.contains('mirroredImage') ? 'scaleX(-1) ' : ''
-            img.style.transform = `${flip}scale(${scale})`
-        }
-
-        let offset = 0
-        applyTransform(sectionImages[0], init + ((scroll - offset) / factor))
-        offset = offset + sectionImages[0].height
-
-        for (let i = 1, len = sectionImages.length; i < len; i++) {
-            applyTransform(sectionImages[i], init + ((scroll - offset) / factor))
-            offset = offset + sectionImages[i].height
-        }
-    })
 
     // Scroll-down button
     const scrollBtn = document.getElementById('scrollDown')
